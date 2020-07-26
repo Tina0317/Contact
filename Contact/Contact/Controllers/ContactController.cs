9b34e85ed123dc1206c28789b777f2b3d43d8558
Tina <ammin0317@gmail.com>
2020-07-26 09:56:01 +0800

    初版

---
+++ Contact/Contact/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using Contact.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Contact.Services;
+
+namespace Contact.Controllers
+{
+    public class ContactController : ApiController
+    {
+    }
+}
---
+++ Contact/Contact/Services/ContactRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Contact.Models;
+
+namespace Contact.Services
+{
+    public class ContactRepository
+    {
+    }
+}
